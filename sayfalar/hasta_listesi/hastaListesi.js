833bb332ec46d74ce83151271b331e5c970046f4
meteAcet <mete.acet@gmail.com>
2024-05-17 00:53:21 +0300

    JSON formatından veri alma eklendi
    JSON formatından veri alma eklendi

--- sayfalar/hasta_listesi/hastaListesi.js
+++ sayfalar/hasta_listesi/hastaListesi.js
@@ -1,20 +1,62 @@
 const ekleButonuDOM = document.getElementById("ekleBtn");
 var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
-// const HastaListesiDOM = document.getElementById("HastaListesi");
 
 
-
-ekleButonuDOM.onclick = function () {
+ekleButonuDOM.onclick = () => {
     modal.style.display = "block";
 }
 
-span.onclick = function () {
+span.onclick = () => {
     modal.style.display = "none";
 }
 
-window.onclick = function (event) {
+window.onclick = (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+
+
+// veri çekip yazdırma 
+const HastaListesiDOM = document.getElementById("HastaListesi");
+
+async function getUserData() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const userList = await response.json();
+        getUserList(userList);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        console.log("işlem tamamlandı.");
+    }
+}
+
+function getUserList(userList) {
+
+    let userTable = "";
+
+    userList.forEach(user => {
+        userTable += `
+        
+                        <tr>
+                            <td>${user.email}</td>
+                            <td>${user.name}</td>
+                            <td>${user.username}</td>
+                            <td>${user.phone}</td>
+                            <td>
+                                <button class="btn btn-primary btn-sm">Düzenle</button>
+                                <button class="btn btn-danger btn-sm">Sil</button>
+                            </td>
+                        </tr>
+
+        `
+
+    });
+
+    HastaListesiDOM.innerHTML = userTable;
+}
+
+getUserData();
