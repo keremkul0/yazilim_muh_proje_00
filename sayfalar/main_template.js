43c2c379dc8b2d81ecc22dadd683d1cc0c10f8c2
keremkul0 <keremkul0@outlook.com>
2024-05-28 23:21:36 +0300

    Hesabım Düzenlendi, Hasta bilgileri güncelleme sayfası güncellendi

--- sayfalar/main_template.js
+++ sayfalar/main_template.js
@@ -1,42 +1,49 @@
 const main_templateDom = document.getElementById("main_template");
 addMainTemplateToDom();
 
-function addMainTemplateToDom(){
-    let template = "";
-    template = `
-
-    <div class=" d-md-block sidebar border-menu">
+function addMainTemplateToDom() {
+    let template = `
+    <div class="sidebar border-menu">
         <div class="position-sticky sidebar-sticky">
             <nav class="LOGO-nav">
                 <a href="/sayfalar/anasayfa_00.html"><img src="../../resimler/toothLogo-removebg-preview.png" class="LOGO"></a>
                 <a class="navbar-brand LOGO-Title" href="/sayfalar/anasayfa_00.html">I-DENTIST</a>
             </nav>
-            <!-- <h4 class="px-3 pt-3" align=center>Menü</h4> -->
             <hr>
             <br>
             <ul class="nav flex-column px-3 ">
                 <li class="nav-item mb-2" align="center">
-                    <a class="nav-link menu-link " href="/sayfalar/hasta_listesi/Hasta_Listesi.html">Hasta
-                        Listesi</a>
+                    <a class="nav-link menu-link" href="/sayfalar/hasta_listesi/Hasta_Listesi.html">Hasta Listesi</a>
                 </li>
                 <li class="nav-item mb-2" align="center">
                     <a class="nav-link menu-link" href="/sayfalar/Randevu_Kayit/Randevu_listesi.html">Randevular</a>
                 </li>
                 <li class="nav-item mb-2" align="center">
-                    <a class="nav-link menu-link"
-                        href="/sayfalar/Gecmis_Randevular/Gecmis-Randevular.html">Geçmiş</a>
+                    <a class="nav-link menu-link" href="/sayfalar/Gecmis_Randevular/Gecmis-Randevular.html">Geçmiş</a>
                 </li>
                 <li class="nav-item mb-2" align="center">
-                <a class="nav-link menu-link"
-                    href="/sayfalar/Hesabim/Hesabim.html">Hesabim</a>
+                    <a class="nav-link menu-link" href="/sayfalar/Hesabim/Hesabim.html">Hesabim</a>
                 </li>
             </ul>
         </div>
-    </div>  
-
-
-    `
+    </div>`;
 
     main_templateDom.innerHTML = template;
     main_templateDom.classList.add("col-lg-2");
+
+    // Add fixed positioning to sidebar
+    const sidebar = main_templateDom.querySelector('.sidebar');
+    sidebar.style.position = 'fixed';
+    sidebar.style.top = '0';
+    sidebar.style.bottom = '0';
+    sidebar.style.overflowY = 'auto';
+    sidebar.style.width = '200px';  // Adjust based on the actual width of your sidebar
 }
+
+// Adjust the content to avoid overlapping with the fixed sidebar
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.querySelector('.content');
+    if (content) {
+        content.style.marginLeft = '200px';  // Adjust based on the actual width of your sidebar
+    }
+});
